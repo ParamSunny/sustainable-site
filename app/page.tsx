3d65f3ad34f54b4698f0b54196898f17b769a76f
ParamSunny <param27singh27@gmail.com>
2025-09-14 19:25:57 +0530

    Create base for start to build the website

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-green-100">
+      <h1 className="text-5xl font-bold text-green-700">
+        Tailwind is Working 🎉
+      </h1>
+    </div>
+  );
+}
